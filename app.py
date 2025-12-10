97b1e3ee9df1479418515f554d5216538e9543b3
Uppumaguluru Aravind <152086701+AravindAravi5@users.noreply.github.com>
2025-12-10 12:48:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.markdown("""
         }
         .card {
             padding: 15px 20px;
-            background-color: #ffffff;
+            background-color: #000000;
             border-radius: 12px;
             margin-bottom: 12px;
             box-shadow: 0 4px 10px rgba(0,0,0,0.08);
